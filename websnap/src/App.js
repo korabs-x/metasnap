948a5f0fa7ecb6e6041ff2be83ae9947657a8806
korabs-x <ko.abstreiter@gmail.com>
2022-03-08 13:25:40 +0100

    indents

--- websnap/src/App.js
+++ websnap/src/App.js
@@ -1,67 +1,69 @@
 import './App.css';
 import {retreiveSnap} from "./retreiveSnap";
-import {getSnapsForURL, storeSnapMetadata} from "./manageSnaps";
+import {getSnapsForURL} from "./manageSnaps";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>
-          Paste url to store a snapshot of it:
-        </h3>
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault();
-            console.log(event.target.url.value)
-            const snap = await retreiveSnap(event.target.url.value);
-            console.log(snap)
-            //const snap = {date: 'nowStr', url: 'url', snapUrl: 'snapUrl'};
-            //storeSnapMetadata(snap);
-          }}
-        >
-          <div style={{fontSize: 12}}>
-            Examples:<br/>
-            https://www.webkitx.com/doc/light/images/WebKitX_Logo_64x64.png<br/>
-            https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt
-          </div>
-          <br/>
-          <input
-            name="url"
-            type="text"
-            placeholder="https://docs.filecoin.io/"
-            defaultValue="https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt"
-          />
-          <button type="submit" className="cta-button submit-gif-button">
-            Snap!
-          </button>
-        </form>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h3>
+                    Paste url to store a snapshot of it:
+                </h3>
+                <form
+                    onSubmit={async (event) => {
+                        event.preventDefault();
+                        console.log(event.target.url.value)
+                        const snap = await retreiveSnap(event.target.url.value);
+                        console.log(snap)
+                        //const snap = {date: 'nowStr', url: 'url', snapUrl: 'snapUrl'};
+                        //storeSnapMetadata(snap);
+                    }}
+                >
+                    <div style={{fontSize: 12}}>
+                        Examples:<br/>
+                        https://www.webkitx.com/doc/light/images/WebKitX_Logo_64x64.png<br/>
+                        https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt
+                    </div>
+                    <br/>
+                    <input
+                        name="url"
+                        type="text"
+                        placeholder="https://docs.filecoin.io/"
+                        defaultValue="https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt"
+                    />
+                    <button type="submit" className="cta-button submit-gif-button">
+                        Snap!
+                    </button>
+                </form>
 
 
-        <h3>
-          Or retrieve snapshots via url:
-        </h3>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log(event.target.url.value)
-            const url = event.target.url.value
-            const snaps = getSnapsForURL(url)
-            console.log(snaps)
-            // TODO: list dates of available snaps with dates
-          }}
-        >
-          <input
-            name="url"
-            type="text"
-            placeholder="https://docs.filecoin.io/"
-          />
-          <button type="submit" className="cta-button submit-gif-button">
-            Go!
-          </button>
-        </form>
-      </header>
-    </div>
-  );
+                <h3>
+                    Or retrieve snapshots via url:
+                </h3>
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        console.log(event.target.url.value)
+                        const url = event.target.url.value
+                        const snaps = getSnapsForURL(url)
+                        console.log(snaps)
+                        // TODO: list dates of available snaps with dates
+                        const dates = snaps.keys();
+
+                    }}
+                >
+                    <input
+                        name="url"
+                        type="text"
+                        placeholder="https://docs.filecoin.io/"
+                    />
+                    <button type="submit" className="cta-button submit-gif-button">
+                        Go!
+                    </button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- websnap/src/manageSnaps.js
+++ websnap/src/manageSnaps.js
@@ -1,17 +1,17 @@
 const testSnaps = {
-  "https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt": {
-    "3/8/2022 12:9:56": {
-      "snapUrl": "https://hub.textile.io/ipfs/bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba"
+    "https://bafzbeibnbpxeejbzvkgb26ex4x6k336gwyr25kzldyxiwdlvalrmbwlpwq.textile.space/test.txt": {
+        "3/8/2022 12:9:56": {
+            "snapUrl": "https://hub.textile.io/ipfs/bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba"
+        },
+        "3/8/2022 12:11:20": {
+            "snapUrl": "https://hub.textile.io/ipfs/bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba"
+        }
     },
-    "3/8/2022 12:11:20": {
-      "snapUrl": "https://hub.textile.io/ipfs/bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba"
+    "https://www.webkitx.com/doc/light/images/WebKitX_Logo_64x64.png": {
+        "3/8/2022 12:10:56": {
+            "snapUrl": "https://hub.textile.io/ipfs/bafkreif5thzztxr62jz7igszjypgtvqwdpdvt4f32j6wz6jf5jncdzkyku"
+        }
     }
-  },
-  "https://www.webkitx.com/doc/light/images/WebKitX_Logo_64x64.png": {
-    "3/8/2022 12:10:56": {
-      "snapUrl": "https://hub.textile.io/ipfs/bafkreif5thzztxr62jz7igszjypgtvqwdpdvt4f32j6wz6jf5jncdzkyku"
-    }
-  }
 }
 const snaps = testSnaps; //{}
 
@@ -33,19 +33,19 @@ const snaps = testSnaps; //{}
 }*/
 
 export const storeSnapMetadata = (snap) => {
-  console.log('storeSnapMetadata')
-  if (!(snap.url in snaps))
-    snaps[snap.url] = {}
-  snaps[snap.url][snap.date] = {snapUrl: snap.snapUrl}
-  console.log(snaps);
+    console.log('storeSnapMetadata')
+    if (!(snap.url in snaps))
+        snaps[snap.url] = {}
+    snaps[snap.url][snap.date] = {snapUrl: snap.snapUrl}
+    console.log(snaps);
 }
 
 export const getSnapsForURL = (url) => {
-  if (!(url in snaps))
-    return {}
-  return snaps[url]
+    if (!(url in snaps))
+        return {}
+    return snaps[url]
 }
 
 export const getAllSnapsURLs = () => {
-  return snaps.keys()
+    return snaps.keys()
 }

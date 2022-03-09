fcc0bda3eda2bbf9dc24452556f20a15ad4d6dad
alex <alexander.abstreiter@gmail.com>
2022-03-09 11:22:54 +0100

    add rinkeby

--- smart/truffle-config.js
+++ smart/truffle-config.js
@@ -1,14 +1,20 @@
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "5777",
     },
-    develop: {
-      port: 8545
-    }
+    rinkeby: { // get eth from https://rinkebyfaucet.com/
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   }
-};
+}
